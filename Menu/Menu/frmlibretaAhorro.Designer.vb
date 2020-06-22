ff929c656c9b18cf2e709a278f6fc55799d9d570
walther efrain <waltherulloa95@gmail.com>
2020-06-22 09:00:21 -0600

    LibretaAhorro

---
+++ Menu/Menu/frmlibretaAhorro.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmlibretaAhorro
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListRetiros = New System.Windows.Forms.ListBox()
+        Me.ListDepositos = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 24)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(374, 145)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingreso de Informacion"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(266, 81)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 46)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Aperturar Cuenta"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(132, 56)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(132, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "monto"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre Usuario"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Location = New System.Drawing.Point(22, 175)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(374, 109)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Opereaciones"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(214, 34)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 45)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Retirar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(40, 34)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 45)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Depositar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Button4)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.ListRetiros)
+        Me.GroupBox3.Controls.Add(Me.ListDepositos)
+        Me.GroupBox3.Location = New System.Drawing.Point(22, 299)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(383, 139)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Historial"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(143, 22)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Retiros"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Depositos"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(288, 94)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(86, 22)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Limpiar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(274, 68)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(294, 43)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Saldo total"
+        '
+        'ListRetiros
+        '
+        Me.ListRetiros.FormattingEnabled = True
+        Me.ListRetiros.Location = New System.Drawing.Point(133, 38)
+        Me.ListRetiros.Name = "ListRetiros"
+        Me.ListRetiros.Size = New System.Drawing.Size(120, 95)
+        Me.ListRetiros.TabIndex = 1
+        '
+        'ListDepositos
+        '
+        Me.ListDepositos.FormattingEnabled = True
+        Me.ListDepositos.Location = New System.Drawing.Point(7, 38)
+        Me.ListDepositos.Name = "ListDepositos"
+        Me.ListDepositos.Size = New System.Drawing.Size(120, 95)
+        Me.ListDepositos.TabIndex = 0
+        '
+        'frmlibretaAhorro
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(420, 450)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmlibretaAhorro"
+        Me.Text = "frmlibretaAhorro"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ListRetiros As ListBox
+    Friend WithEvents ListDepositos As ListBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Menu/Menu/frmlibretaAhorro.vb
@@ -0,0 +1,74 @@
+﻿Public Class frmlibretaAhorro
+    'procedimientos
+    Private monto As Integer
+    Private Sub activarControles()
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = True
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub desactivarControles()
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub limpiar()
+        desactivarControles()
+        TextBox1.Clear()
+        TextBox2.Clear()
+        ListDepositos.Items.Clear()
+        ListRetiros.Items.Clear()
+    End Sub
+
+    Private Sub frmlibretaAhorro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        desactivarControles()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cliente As String
+        cliente = TextBox1.Text
+        monto = Val(TextBox2.Text)
+        If (monto > 0) Then
+            activarControles()
+        Else
+            MessageBox.Show("monto mayor a 0", "Ingresar monto correcto ", MessageBoxButtons.OK)
+        End If
+    End Sub
+    Private Sub mostrarsaldo()
+        TextBox2.Text = monto
+
+    End Sub
+    Private Function leer(mensaje As String)
+        Dim cantidad As Double
+        cantidad = InputBox("ingrese un monto" & mensaje, "Operacion")
+        mostrarsaldo()
+        Return cantidad
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim deposito As Double
+        deposito = leer("depositar")
+
+        monto += deposito
+        ListDepositos.Items.Add(deposito)
+        mostrarsaldo()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim retiro As Double
+        retiro = leer("Retirar")
+        If (retiro > monto) Then
+            MessageBox.Show("saldo insuficiente", "deposite mas", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            monto -= retiro
+            ListRetiros.Items.Add(retiro)
+            mostrarsaldo()
+        End If
+    End Sub
+End Class
